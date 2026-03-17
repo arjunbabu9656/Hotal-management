96d113167dd9e3a32de2c370af136240030a8b00
arjunbabu9656 <arjunb7025@gmail.com>
2026-03-17 14:50:49 +0530

    Deploy: Automate superuser creation for free tier

--- build.sh
+++ build.sh
@@ -6,3 +6,8 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Create superuser if environment variables are set
+if [[ $DJANGO_SUPERUSER_USERNAME ]]; then
+  python manage.py createsuperuser --no-input || true
+fi
